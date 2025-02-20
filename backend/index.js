b7bc4124ecd8e067611ac54168286ebe75421fe0
Stav Shlimovich <stavshlimovich@Stavs-MacBook-Air.local>
2025-02-21 01:01:53 +0200

    add monitoring

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,57 @@
-const cors = require('cors');
 const express = require('express');
+const cors = require('cors');
+const promClient = require('prom-client');
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-// Enable CORS for all origins (for development)
-app.use(cors({
-  origin: '*', // Or set to 'http://localhost:8080' to allow only the frontend
-}));
+// cors use
+app.use(cors());
 
-app.get('/api/message', (req, res) => {
-    res.json({ message: 'Hola Esh tester from Backend!' });
+// Prometheus metrics
+const register = promClient.register;
+
+// Metrics
+const httpRequestDurationMicroseconds = new promClient.Histogram({
+  name: 'http_request_duration_seconds',
+  help: 'Histogram of HTTP request duration in seconds',
+  buckets: [0.1, 0.2, 0.5, 1, 2, 5]
 });
 
+const httpRequestCount = new promClient.Counter({
+  name: 'http_request_count',
+  help: 'Total HTTP requests made'
+});
+
+const httpRequestErrorCount = new promClient.Counter({
+  name: 'http_request_error_count',
+  help: 'Total number of errors in HTTP requests'
+});
+
+// metrics route for Prometheus scraping
+app.get('/metrics', async (req, res) => {
+  try {
+    res.set('Content-Type', register.contentType);
+    res.end(await register.metrics());
+  } catch (ex) {
+    res.status(500).end(ex);
+  }
+});
+
+app.get('/api/message', (req, res) => {
+  const end = httpRequestDurationMicroseconds.startTimer();
+  httpRequestCount.inc(); // ncrement request count
+
+  //error for testing
+  if (Math.random() < 0.1) {
+    httpRequestErrorCount.inc(); // iincrement error count
+    return res.status(500).json({ error: 'Something went wrong!' });
+  }
+
+  
+  res.json({ message: 'Hola Esh tester from Backend!' });
+  end(); // request duration
+});
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Backend's home port is ${PORT}`);
+  console.log(`Backend's home port is ${PORT}`);
 });
